f26640c188d4dc55f0345fede2770ee6d9ba944f
LeRoyce Pearson <leroycepearson@geemili.xyz>
2018-08-21 20:00:44 -0500

    Start drafting api ideas for core

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -11,3 +11,59 @@ pub mod parse;
 pub mod split;
 pub mod interval_tree;
 
+
+use group::Group;
+use std::collections::HashMap;
+
+struct Procedure {
+    id: usize,
+    name: String,
+}
+
+struct Request {
+    group_id: usize,
+    procedure_id: usize,
+    amount: usize,
+}
+
+pub struct Core {
+    groups: Vec<Group>,
+    procedures: HashMap<usize, String>,
+    requests: Vec<Request>,
+    // Metadata
+    procedure_next_id: usize,
+}
+
+impl Core {
+    pub fn new() -> Self {
+        Core {
+            groups: vec![],
+            procedures: HashMap::new(),
+            requests: vec![],
+            procedure_next_id: 0,
+        }
+    }
+
+    pub fn set_groups(&mut self, groups: Vec<Group>) {
+        self.groups = groups;
+    }
+
+    pub fn add_procedure(&mut self) -> usize {
+        let id = self.procedure_next_id;
+        let name = String::new();
+        self.procedure_next_id += 1;
+        self.procedures.insert(id, name);
+        id
+    }
+
+    pub fn get_procedure_name(&self, id: usize) -> Option<&String> {
+        self.procedures.get(&id)
+    }
+
+    pub fn set_procedure_name(&mut self, id: usize, text: String) {
+        if self.procedures.get(&id).is_none() {
+            return;
+        }
+        self.procedures.insert(id, text);
+    }
+}
