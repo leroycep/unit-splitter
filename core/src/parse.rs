e161851af569f5836247004b325298cf4522a223
LeRoyce Pearson <leroycepearson@geemili.xyz>
2018-08-21 21:33:58 -0500

    Write tokenizer logic
    Still need to write logic to collect errors from tokenization

--- core/src/parse.rs
+++ core/src/parse.rs
@@ -3,6 +3,139 @@ include!(concat!(env!("OUT_DIR"), "/notation.rs"));
 use ::interval_tree::IntervalTreeNode;
 use std::collections::HashSet;
 
+
+#[derive(Eq, PartialEq, Debug, Clone)]
+pub struct Token {
+    start: usize,
+    len: usize,
+    kind: TokenKind,
+}
+
+impl Token {
+    pub fn new(start: usize, len: usize, kind: TokenKind) -> Self {
+        Self {
+            start,
+            len,
+            kind,
+        }
+    }
+}
+
+#[derive(Eq, PartialEq, Debug, Copy, Clone)]
+pub enum TokenKind {
+    Equals,
+    Hyphen,
+    Number,
+    Identifier,
+}
+
+struct Tokenizer {
+    source: Vec<(usize, char)>,
+    pos: usize,
+}
+
+impl Tokenizer {
+    pub fn new(source: &str) -> Self {
+        Self {
+            source: source.char_indices().collect(),
+            pos: 0,
+        }
+    }
+
+    pub fn get_next_token(&mut self) -> Option<Token> {
+        loop {
+            match self.next_char() {
+                None => return None,
+                Some((pos, '=')) => return Some(Token::new(pos, 1, TokenKind::Equals)),
+                Some((pos, '-')) => return Some(Token::new(pos, 1, TokenKind::Hyphen)),
+                Some((_, c)) if is_whitespace(c) => continue,
+                Some((start_pos, c)) if c.is_digit(10) => {
+                    let mut c;
+                    let mut is_number = true;
+                    loop {
+                        c = match self.peek_char() {
+                            None => break,
+                            Some((_pos, c)) => c
+                        };
+                        if c.is_digit(10) { }
+                        else if is_whitespace(c) { break }
+                        else if is_hyphen(c) { break }
+                        else if is_identifier(c) { is_number = false; }
+                        else { /* TODO: Report error, unexpected character */ }
+                        self.next_char(); // Consume character if loop has not broke yet
+                    }
+                    let len = self.pos - start_pos;
+                    return Some(if is_number {
+                        Token::new(start_pos, len, TokenKind::Number)
+                    } else {
+                        Token::new(start_pos, len, TokenKind::Identifier)
+                    });
+                },
+                Some((start_pos, c)) if is_identifier(c) => {
+                    let mut c;
+                    loop {
+                        c = match self.peek_char() {
+                            None => break,
+                            Some((_pos, c)) => c
+                        };
+                        if is_identifier(c) { }
+                        else if is_whitespace(c) { break }
+                        else if is_equals(c) { break }
+                        else { /* TODO: Report error, unexpected character */ }
+                        self.next_char(); // Consume character if loop has not broke yet
+                    }
+                    let len = self.pos - start_pos;
+                    return Some(Token::new(start_pos, len, TokenKind::Identifier));
+                },
+                Some((_pos, _c)) => {
+                    // TODO: Report error, unexpected character
+                }
+            }
+        }
+    }
+
+    //fn get_number(&mut self)
+
+    fn next_char(&mut self) -> Option<(usize, char)> {
+        let result = self.source.get(self.pos).map(|(p, c)| (*p, *c));
+        if result.is_some() {
+            self.pos += 1;
+        }
+        result
+    }
+
+    fn peek_char(&self) -> Option<(usize, char)> {
+        self.source.get(self.pos).map(|(p, c)| (*p, *c))
+    }
+}
+
+pub fn tokenize(source: &str, output: &mut Vec<Token>) {
+    let mut tokenizer = Tokenizer::new(source);
+    while let Some(token) = tokenizer.get_next_token() {
+        output.push(token);
+    }
+}
+
+pub fn is_equals(c: char) -> bool {
+    c == '='
+}
+
+pub fn is_hyphen(c: char) -> bool {
+    c == '-'
+}
+
+pub fn is_whitespace(c: char) -> bool {
+    c == ' '
+    || c == '\n'
+    || c == '\r'
+    || c == '\t'
+    || c == ','
+}
+
+pub fn is_identifier(c: char) -> bool {
+    c.is_alphanumeric() || c == '-'
+}
+
 pub fn find_overlaps(ranges: &[Range]) -> HashSet<(Range, Range)> {
     assert!(ranges.len() > 0);
     let mut tree = IntervalTreeNode::new(ranges[0].clone());
@@ -126,4 +259,30 @@ mod tests {
 
         assert_eq!(overlaps, conflicts);
     }
+
+    #[test]
+    fn tokenize_first_group_is_unnamed() {
+        use ::parse::Token;
+        use ::parse::TokenKind::*;
+        let text = "1-50, B=51-100, C=101-150";
+        let expected = vec![
+            Token::new(0, 1, Number),
+            Token::new(1, 1, Hyphen),
+            Token::new(2, 2, Number),
+            Token::new(6, 1, Identifier),
+            Token::new(7, 1, Equals),
+            Token::new(8, 2, Number),
+            Token::new(10, 1, Hyphen),
+            Token::new(11, 3, Number),
+            Token::new(16, 1, Identifier),
+            Token::new(17, 1, Equals),
+            Token::new(18, 3, Number),
+            Token::new(21, 1, Hyphen),
+            Token::new(22, 3, Number),
+        ];
+        let mut tokens = Vec::new();
+        ::parse::tokenize(text, &mut tokens);
+        assert_eq!(tokens, expected);
+    }
+
 }
