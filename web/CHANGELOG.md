b93e81c50fba6af989b711d9c41cc2603ac3be23
LeRoyce Pearson <leroycepearson@geemili.xyz>
2018-10-13 21:04:25 -0500

    Update changelog
    And add styling and whatnot.

--- web/CHANGELOG.md
+++ web/CHANGELOG.md
@@ -5,10 +5,17 @@ All notable changes to the [Unit Splitter][unit-splitter] project will be docume
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/)
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
-## Unreleased
+## 0.2.0 - 2018-10-13
 ### Added
+- Requests text input
 - Indication of software version
 - Link to changelog
+
+### Changed
+- Moved to a new design for the page that is more responsive
+
+### Removed
+- Test/Request button based UI
 
 ## 0.1.0 - 2018-06-06
 ### Added
--- web/generate-static-files.sh
+++ web/generate-static-files.sh
@@ -1,4 +1,32 @@
 #!/usr/bin/env bash
 
 echo "Generating changelog.html"
-comrak CHANGELOG.md > static/changelog.html
+
+OUT=static/changelog.html
+
+echo "" > $OUT
+echo '
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta charset="utf-8">
+        <title>Unit Splitter - Changelog</title>
+        <link rel="stylesheet" href="reset.css">
+        <link rel="stylesheet" href="styles.css">
+    </head>
+    <body class="container">
+        <header>Unit Splitter - Changelog</header>
+        <nav></nav>
+        <main>
+' >> $OUT
+
+comrak CHANGELOG.md >> $OUT
+
+echo '
+        </main>
+        <aside></aside>
+        <footer>LeRoyce Pearson &lt;leroycepearson@geemili.xyz&gt;</footer>
+    </body>
+</html>
+' >> $OUT
